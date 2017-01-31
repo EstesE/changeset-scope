77642216a323b9d4989a11879594f5d36e612907
Eric Estes <eric_estes@aspensquare.com>
2017-01-31 17:52:45 -0500

    Added the removeAddress and addAddress actions.

--- app/components/my-addresses.js
+++ app/components/my-addresses.js
@@ -1,15 +1,31 @@
 import Ember from 'ember';
 
+const { isPresent } = Ember;
+
 export default Ember.Component.extend({
 
     actions: {
 
-        removeAddress:function() {
+        removeAddress:function(address) {
             console.log('Remove Address');
+            if (isPresent(address)) {
+                let addresses = this.get('model');
+                addresses.removeObject(address);
+            }
         },
 
         addAddress: function() {
             console.log('Add Address');
+            let addresses = this.get('model');
+            let address = Ember.Object.create({
+                street: '',
+                city: '',
+                state: '',
+                zip: '',
+                isCurrent: false,
+                rent: false
+            });
+            addresses.pushObject(address);
         }
     }
 });
