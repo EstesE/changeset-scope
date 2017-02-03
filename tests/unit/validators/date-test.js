351c483666a704d5dd4e19e7b3a882a0adf8a79a
Eric Estes <eric_estes@aspensquare.com>
2017-02-03 11:52:44 -0500

    Remove two validation files that were not needed.

--- tests/unit/validators/date-test.js
+++
@@ -1,8 +0,0 @@
-import { module, test } from 'qunit';
-import validateDate from 'changeset-scope/validators/date';
-
-module('Unit | Validator | date');
-
-test('it exists', function(assert) {
-  assert.ok(validateDate());
-});
